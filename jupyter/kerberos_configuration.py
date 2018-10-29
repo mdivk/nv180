c100f49f3bd5b0703e7ef9c770588083c8019c0d
mdivk <mdivk@users.noreply.github.com>
2018-10-29 14:33:40 -0400

    Update kerberos_configuration.py

--- jupyter/kerberos_configuration.py
+++ jupyter/kerberos_configuration.py
@@ -4,8 +4,10 @@ from subprocess import check_call
 import os
 def my_kinit(spawner):
     username = spawner.user.name # get the username
-    script = '/usr/bin/kinit_jupyter_user.sh'
-    check_call([script, username])
+    sudo='/bin/sudo/' #run the check_call with sudo
+    script = '/usr/local/bin/kinit_jupyter_user.sh'
+    check_call([sudo, script, username]) #running with sudo ensure accessible to log
 
 # attach the hook function to the spawner
 c.Spawner.pre_spawn_hook = my_kinit
+
